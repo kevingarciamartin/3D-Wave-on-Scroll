73131c8e0f1a7a14e5bdf8fde26c638e4f50c72c
Kevin Garcia Martin <kevingm@live.se>
2026-05-04 16:53:36 +0200

    fix: change wave function

--- src/components/shader.tsx
+++ src/components/shader.tsx
@@ -7,7 +7,12 @@ export const vertex = `
   void main() {
     vUv = uv;
     vec3 newPosition = position;
-    float wave = uAmplitude * sin(position.x * uWaveLength + uTime) + uAmplitude * cos(position.y * uWaveLength + uTime);
+    float wave = uAmplitude * sin(position.x * uWaveLength + uTime);
+    wave += uAmplitude * sin(0.8 * position.x * uWaveLength + uTime * 0.7);
+    wave += uAmplitude * sin(0.5 * position.x * uWaveLength + uTime * 0.5);
+    wave += uAmplitude * sin(2.1 * position.x * uWaveLength + uTime * 0.1);
+    wave += uAmplitude * sin(0.3 * position.y * uWaveLength + uTime * 0.9);
+    wave += uAmplitude * sin(1.3 * position.y * uWaveLength + uTime * 0.3);
     newPosition.z += wave;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
   }
